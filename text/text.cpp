691ca96cd2cfee5e2f7407440d1fa808d08e7f9a
Denis <denfill008@mail.ru>
2023-12-17 15:32:16 +0300

    Добавьте файлы проекта.

---
+++ text/text.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+using namespace std;
+
+string text_compression(string a) {
+    string buf = "";
+    bool counter = true;
+    
+    for (int i = 0; i < a.size();i++)
+    {
+        if (a[i] == ' ' && counter == true)
+        {
+            buf += a[i];
+            counter = false;
+            continue;
+        }
+
+        if (a[i] != ' ')
+        {
+            buf += a[i];
+            counter = true;
+        }
+    }
+    return buf;
+}
+
+int main()
+{
+    string a = "asdasd     asdasd sadasd asd as d as dasdasd as d    asd";
+    cout << text_compression(a);
+}
+
